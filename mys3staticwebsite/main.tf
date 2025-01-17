a709ebd136202c9cfa35a7d500dcc78f2ae3d69b
Sandip Kumar Dey <96774612+sandipkumardey@users.noreply.github.com>
2025-01-18 00:57:16 +0530

    added the app files and initial terraform setup

---
+++ mys3staticwebsite/main.tf
@@ -0,0 +1,32 @@
+#create s3 bucket
+resource "aws_s3_bucket" "newbucket" {
+  bucket = var.bucketname
+}
+
+resource "aws_s3_bucket_ownership_controls" "example" {
+  bucket = aws_s3_bucket.newbucket.id
+
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.newbucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "example" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.example,
+    aws_s3_bucket_public_access_block.example,
+  ]
+
+  bucket = aws_s3_bucket.newbucket.id
+  acl    = "public-read"
+}
+
